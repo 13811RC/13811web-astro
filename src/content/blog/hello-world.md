b9d5c5d8540d56c69984aada60d2bbc9da74e718
EchoEndr <69995058+EchoEndr@users.noreply.github.com>
2025-03-25 15:34:57 -0500

    blog

---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,19 @@
+---
+title: "Hello World"
+description: "awesome blog post"
+pubDate: "2025-03-21"
+updatedDate: "2025-03-21"
+heroImage: "/blog-placeholder-1.jpg"
+---
+
+# Awesome markdown support
+
+shout out Astro from **supporting** mark*down*
+
+## Code block
+
+```ts
+const sigma = "sigma";
+console.log(sigma);
+```
+
---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    draft: z.boolean().optional().default(false),
+  }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+};
